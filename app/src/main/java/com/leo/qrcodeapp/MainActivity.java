227fb1072d5ed0f949b17329b3c5343e6d862ab0
ciatph <ciat.ph@gmail.com>
2018-08-05 17:27:33 +0800

    Used old gms vision barcode reader. Used native device camera for qrcode capture.

--- app/src/main/java/com/leo/qrcodeapp/MainActivity.java
+++ app/src/main/java/com/leo/qrcodeapp/MainActivity.java
@@ -1,13 +1,60 @@
 package com.leo.qrcodeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.leo.qrcodeapp.barcode.BarcodeCaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Text from scanned QR code
+        textResult = findViewById(R.id.txt_result);
+
+        // Add click event to the camera button
+        findViewById(R.id.btn_camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startCamera();
+            }
+        });
+    }
+
+    /**
+     * Start the native device camera
+     */
+    private void startCamera(){
+        Intent intent = new Intent(this, BarcodeCaptureActivity.class);
+        startActivityForResult(intent, StatusCodes.INSTANCE.BARCODE_READER_REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        String message = "";
+
+        if(requestCode == StatusCodes.INSTANCE.BARCODE_READER_REQUEST_CODE && resultCode == CommonStatusCodes.SUCCESS){
+            if(data != null){
+                Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                Log.d("-scan result", barcode.displayValue + "!");
+                message = barcode.displayValue;
+                textResult.setText(barcode.displayValue);
+            }
+
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/leo/qrcodeapp/StatusCodes.java
@@ -0,0 +1,7 @@
+package com.leo.qrcodeapp;
+
+public enum StatusCodes {
+    INSTANCE;
+
+    final int BARCODE_READER_REQUEST_CODE = 1;
+}
